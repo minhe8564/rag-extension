bada7fd433a51a39d37a5de71e137e42a0e32b1a
최민석 <gd10080008@gmail.com>
2025-10-31 11:44:46 +0900

    [INFRA] feat: Jenkins 연결 && Docker 이미지화

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import __version__, __title__, __description__
+from .config import settings
 from datetime import datetime
 
 app = FastAPI(
     title=__title__,
     description=__description__,
     version=__version__,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.allowed_origins_list,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
