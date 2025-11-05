5ee7f2d8cb386f968046edfc0e9f0e49a49b4b40
백종석 <baekjs0123@naver.com>
2025-11-05 17:19:54 +0900

    [BE] feat: Query템플릿 CRUD 구현

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ from .domains.file.routers.file_category import router as file_router
 from .domains.image.routers.image_controller import router as image_router
 from .domains.rag_setting.routers.strategy_router import router as strategy_router
 from .domains.rag_setting.routers.ingest_router import router as ingest_router
+from .domains.rag_setting.routers.query_router import router as query_router
 from .domains.monitoring.routers.monitoring_controller import router as monitoring_router
 from datetime import datetime
 
@@ -79,6 +80,7 @@ app.include_router(file_router, prefix="/api/v1")
 app.include_router(image_router, prefix="/api/v1")
 app.include_router(strategy_router, prefix="/api/v1")
 app.include_router(ingest_router, prefix="/api/v1")
+app.include_router(query_router, prefix="/api/v1")
 app.include_router(monitoring_router, prefix="/api/v1")
 
 @app.get("/")
