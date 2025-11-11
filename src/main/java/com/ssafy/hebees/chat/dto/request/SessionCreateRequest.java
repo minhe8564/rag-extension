3dc271a22f97d6640944c795205342e58324a087
배준수 <jsbae1023@gmail.com>
2025-11-11 11:33:46 +0900

    [🔀 BE] 세션 생성/수정 중복 파라미터 제거

--- src/main/java/com/ssafy/hebees/chat/dto/request/SessionCreateRequest.java
+++ src/main/java/com/ssafy/hebees/chat/dto/request/SessionCreateRequest.java
@@ -2,7 +2,6 @@ package com.ssafy.hebees.chat.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Size;
-import java.util.UUID;
 
 @Schema(description = "세션 생성 DTO")
 public record SessionCreateRequest(
@@ -10,11 +9,8 @@ public record SessionCreateRequest(
     @Schema(description = "세션 제목", example = "래그프레스 루틴", minLength = 1, maxLength = 50, nullable = true, defaultValue = "새 채팅")
     String title,
 
-    @Schema(description = "사용할 LLM 식별자", format = "uuid", example = "bc4b6766-b861-4ad0-959b-f170767875da", nullable = true)
-    UUID llm,
-
     @Schema(description = "사용할 LLM 이름", example = "GPT-4o", nullable = true)
-    String llmName,
+    String llm,
 
     @Schema(description = "초기 질문(세션명 힌트)", example = "2주뒤에 삼성전자에 가려면 오늘부터 래그프레스를 하루에 몇번씩 해야해?", nullable = true)
     String query
